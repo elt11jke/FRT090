aabea5e57fe15930d81cf8d3b9e4a1f3178c77f6
Kenan Sulejmanovic <elt11ssu@student.lu.se>
2014-11-17 18:34:50 +0100

    Johans fantastiska resa

---
+++ Dokumentation/Modellering och design/modellinertia2.m
@@ -0,0 +1,31 @@
+%Modell för inverted pendulum (inertia hjulet)
+
+s=tf('s');
+
+%Konstanter
+g= 9.81;
+
+%Parametrar som vi ändrar
+m= 0.70;
+M= 0.10;
+l= 0.15;
+L= 0.30;
+R= 0.20;
+Jp= m*(l^2);
+Jw= (1/2)*M*(R^2);
+
+a= (g/Jp)*(l*m + L*M);
+b1= 1/(Jp+Jw + M*(L^2));
+b2= 1/Jw;
+
+
+%Matriser
+A=[0 1 0 0; a 0 0 0; 0 0 0 1; 0 0 0 0];
+B= [0; b1; 0; -b2];
+B_d = [0; 1; 0; 0] 
+C=eye(4);
+D=0;
+
+%Systemet
+SYSinertia= ss(A,[B B_d],C,D);
+Pin = zpk(tf(SYSinertia))
