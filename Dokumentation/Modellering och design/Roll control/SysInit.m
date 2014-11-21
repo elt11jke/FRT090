754f3631478648734e263d4562afff03efffc528
Projektanvändare <frt090_B@project-3.control.lth.se>
2014-11-21 16:45:37 +0100

    senaste designen

--- Dokumentation/Modellering och design/Roll control/SysInit.m
+++ Dokumentation/Modellering och design/Roll control/SysInit.m
@@ -4,10 +4,9 @@ close all
 modelinertia
 Cindesign
 
-x10=0.02;
+x10=deg2rad(2);
 x20=0;
 x30=0;
-x40=0;
 
 T= minreal(zpk(Cin*Pin(1,1)/(1+ Cin*Pin(1,1))))
 
@@ -16,13 +15,13 @@ S= minreal(zpk(1/(1+ Cin*Pin(1,1))))
 pole(T)
 
 
-%Complementary sensitivity function plots
-figure(1)
-nyquist(T)
-
-figure(2)
-margin(T)
-
-%Sensitivity function plots
-figure(3)
-bode(S)
+% %Complementary sensitivity function plots
+% figure(1)
+% nyquist(T)
+% 
+% figure(2)
+% margin(T)
+% 
+% %Sensitivity function plots
+% figure(3)
+% bode(S)
--- Dokumentation/Modellering och design/Roll control/modelinertia.m
+++ Dokumentation/Modellering och design/Roll control/modelinertia.m
@@ -7,31 +7,32 @@ g= 9.81;
 
 %Parametrar som vi Ã¤ndrar
 m= 0.70;
-M= 0.10;
+M= 0.15;
 l= 0.15;
 L= 0.30;
-R= 0.20;
+R= 0.10;
 Jp= m*(l^2);
 Jw= (1/2)*M*(R^2);
 
 a= (g/Jp)*(l*m + L*M);
 b1= 1/(Jp+Jw + M*(L^2));
 b2= 1/Jw;
+ki=1/20;
 
-%Tillstånd
+%Tillstï¿½nd
 %x1: Pendelns vinkel
 %x2: Pendelns vinkelhastighet
 %x3: Motorns vinkel
 %x4: Motorns vinkelhastighet
 
 %Matriser
-A=[0 1 0 0; a 0 0 0; 0 0 0 1; 0 0 0 0];
-B_tau= [0; b1; 0; -b2];
-B_d = [0; 1; 0; 0];
+A=[0 1 0; a 0 0; 0 0 0];
+B_tau= [0; -b1; b2];
+B_d = [0; 1; 0];
 B= [B_tau B_d]
-Cangle=[1 0 0 0];
-Call= eye(4);
-D=zeros(4,2);
+Cangle=[1 0 0];
+Call= eye(3);
+D=zeros(3,2);
 
 %Systemet
 C=Call;
@@ -40,7 +41,7 @@ Pin = zpk(tf(SYSinertia));
 
 
 %State feedback
-Q=eye(4);
+Q=eye(3);
 R=[1 0; 0 100];
 lr=1;
 
